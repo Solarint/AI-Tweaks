4d2755104093061fb77e538c923ac997981f6870
Solarint <127286855+Solarint@users.noreply.github.com>
2023-03-10 12:06:46 -0600

    Update to 0.0.2
    Added distance based fire-rate scaling for all bots. Logic was previously locked to sniper scavs, but is now free for all!
    Added config options in f12 menu with toggles for each features and distance threshold settings.
    For Modders:
    "WAIT_NEXT_SINGLE_SHOT_LONG_MAX", "WAIT_NEXT_SINGLE_SHOT_LONG_MIN", and "MARKSMAN_DIST_SEK_COEF" are now being used for all bots!
    "MARKSMAN_DIST_SEK_COEF" = how many seconds of "WAIT_TIME" are added per x meters. So a setting of 100 means there will be around 1 second between shots when shooting a target at 100 meters with +- 30% with randomization. MAX and MIN are upper and lower limits for the wait time. Recommend very low minimum setting (0.1) and around 3 second Max.

--- Patch.cs
+++ Patch.cs
@@ -12,8 +12,12 @@ using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
 using EFT.InventoryLogic;
+using EFT.Game.Spawning;
+using EFT.Interactive;
+using UnityEngine.AI;
 
-    public class FireratePatch : ModulePatch
+//Swaps Bot Firemode based on distance
+public class Firemode : ModulePatch
     {
         // Temporary debug dictionary, delete it and its usage in a production delivery
         //static Dictionary<string, bool> botmap = new Dictionary<string, bool>();
@@ -48,26 +52,50 @@ using EFT.InventoryLogic;
         Weapon weapon = ___botOwner_0.WeaponManager.CurrentWeapon;
             if (!weapon.WeapFireType.Contains(Weapon.EFireMode.single) || !weapon.WeapFireType.Contains(Weapon.EFireMode.fullauto))
             {
-                //if (!botmap.ContainsKey(___botOwner_0.name))
-                //{
-                    //Logger.LogInfo($"FireratePatch [{___botOwner_0.name}]: Weapon {weapon.Name.Localized(null)} fire types [{weapon.WeapFireType.CastToStringValue(", ")}], ignoring");
-                    //botmap.Add(___botOwner_0.name, true);
-                //}
                 return;
             }
 
             // If the target is far away, and we're not already set to semi auto, switch to semi auto
-            if (___botOwner_0.AimingData.LastDist2Target > 22f && weapon.SelectedFireMode != Weapon.EFireMode.single)
+            if (___botOwner_0.AimingData.LastDist2Target > SAIN.SAIN.SemiDistance.Value && weapon.SelectedFireMode != Weapon.EFireMode.single)
             {
                 Logger.LogDebug($"FireratePatch [{___botOwner_0.name}]: Target is > 20f away, switching to single fire");
                 weapon.FireMode.SetFireMode(Weapon.EFireMode.single);
             }
 
             // If the target is close by, and we're not already set to full auto, switch to full auto
-            if (___botOwner_0.AimingData.LastDist2Target <= 18f && weapon.SelectedFireMode != Weapon.EFireMode.fullauto)
+            if (___botOwner_0.AimingData.LastDist2Target <= SAIN.SAIN.AutoDistance.Value && weapon.SelectedFireMode != Weapon.EFireMode.fullauto)
             {
                 Logger.LogDebug($"FireratePatch [{___botOwner_0.name}]: Target is <= 20f away, switching to auto");
                 weapon.FireMode.SetFireMode(Weapon.EFireMode.fullauto);
             }
         }
+    }
+//Allows all bots to use Sniper Scav Logic for scaling fire-rate at distance.
+public class SemiROF : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+            {
+                //return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("method_5");
+                //return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("Shoot");
+                return typeof(GClass544).GetMethod("method_5", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+
+        [PatchPrefix]
+         public static bool PatchPrefix(ref BotOwner ___botOwner_0, ref float ___float_4)
+            {
+		        if (!___botOwner_0.IsRole(WildSpawnType.marksman))
+		            {
+                        float num = ___botOwner_0.WeaponManager.WeaponAIPreset.HoldTriggerUpTime();
+			            //If target is greater than 20m away, use additional fire-rate calculations previously reserved for sniper scavs
+                        if (___botOwner_0.AimingData.LastDist2Target > SAIN.SAIN.DistanceScalingStart.Value)
+                            {
+                                num = Mathf.Clamp((___botOwner_0.AimingData.RealTargetPoint - ___botOwner_0.Transform.position).magnitude / ___botOwner_0.Settings.FileSettings.Shoot.MARKSMAN_DIST_SEK_COEF, ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MIN, ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MAX);
+                            }
+                        ___float_4 = Time.time + num * UnityEngine.Random.Range(0.5f, 1.5f);
+                        Logger.LogDebug($"Sniper Logic [{___botOwner_0.name}]: Enabling Sniper Logic for fire-rate");
+                        return false;
+		            }
+                Logger.LogDebug($"Sniper Logic [{___botOwner_0.name}]: Ignoring bot for sniper logic");
+                return true;
+            }    
     }
--- Plugin.cs
+++ Plugin.cs
@@ -13,23 +13,33 @@ using EFT.InventoryLogic;
 using System.Reflection;
 using Aki.Reflection.Patching;
 
-namespace AI_Tweaks
+namespace SAIN
 {
     [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
     [BepInProcess("EscapeFromTarkov.exe")]
-
-    public class AI_Tweaks : BaseUnityPlugin
+    public class SAIN : BaseUnityPlugin
     {
         private const String MOD_GUID = "me.sol.aitweaks";
-        private const String MOD_NAME = "Solarint's AI Tweaks";
-        private const String MOD_VERSION = "0.1";
+        private const String MOD_NAME = "SAIN";
+        private const String MOD_VERSION = "0.2";
 
-        public static AI_Tweaks Instance { get; private set; }
+        public static ConfigEntry<bool> Firemode { get; private set; }
+        public static ConfigEntry<bool> SemiROF { get; private set; }
+        public static ConfigEntry<float> SemiDistance { get; private set; }
+        public static ConfigEntry<float> AutoDistance { get; private set; }
+        public static ConfigEntry<float> DistanceScalingStart { get; private set; }
 
         public void Awake()
             {
-                new FireratePatch().Enable();
-               // new RoF().Enable();
-        }
+                Firemode = Config.Bind("Firemode Swap Settings", "Firemode Toggle", true);               
+                SemiDistance = Config.Bind("Firemode Swap Settings", "Maximum Distance where bots will swap to Semi-Auto", 35f);
+                AutoDistance = Config.Bind("Firemode Swap Settings", "Minimum Distance where bots will swap back to Full-Auto", 30f);
+
+                SemiROF = Config.Bind("Semiauto Distance Scaling Settings", "Distance Scaling Toggle", true);
+                DistanceScalingStart = Config.Bind("Semiauto Distance Scaling Settings", "Distance to start slowing down Semi-Auto Firerate", 30f);
+                
+                new Firemode().Enable();
+                new SemiROF().Enable();
+            }
     }
 }
