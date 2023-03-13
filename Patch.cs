d94746578125b471be1fd23a861f684ae11edea2
Solarint <127286855+Solarint@users.noreply.github.com>
2023-03-13 17:46:49 -0500

    Updated to 0.3 with new features
    Recoil modifiers for firemode swap and distance scaling. New config options. Difficulty setting overrides.

--- Patch.cs
+++ Patch.cs
@@ -19,83 +19,95 @@ using UnityEngine.AI;
 //Swaps Bot Firemode based on distance
 public class Firemode : ModulePatch
     {
-        // Temporary debug dictionary, delete it and its usage in a production delivery
         //static Dictionary<string, bool> botmap = new Dictionary<string, bool>();
 
         // Target the "Shoot" method of the class assigned to "ShootData" in the "BotOwner" class
-        // This should hopefully be future proof
         protected override MethodBase GetTargetMethod()
         {
             return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("Shoot");
         }
 
-        // Apply our patch before the "Shoot" method executes
         // The parameter gives us access to the member variable "botOwner_0" in GClass541 that would normally be private
         [PatchPrefix]
         public static void PatchPrefix(ref BotOwner ___botOwner_0)
         {
-            // If aimingData is null, don't do anything
             if (___botOwner_0.AimingData == null)
             {
                 return;
             }
 
-            // Buffer zone for Range to target decision. If aimingData is < 22 and > 18, do nothing.
-            // Will add to this later to adjust firerate in a linear way when on single fire to avoid abrupt changes in fire-rate
-            //if (___botOwner_0.AimingData.LastDist2Target < 22f && ___botOwner_0.AimingData.LastDist2Target > 18f)
-            //{
-            //    Logger.LogInfo($"FireratePatch [{___botOwner_0.name}]: Adjusting Single Fire Rate Based on Distance");
-            //    return;
-            //}
-
-        // If the target weapon doesn't support both semiauto and fullauto, don't bother trying to change it
         Weapon weapon = ___botOwner_0.WeaponManager.CurrentWeapon;
             if (!weapon.WeapFireType.Contains(Weapon.EFireMode.single) || !weapon.WeapFireType.Contains(Weapon.EFireMode.fullauto))
             {
                 return;
             }
 
-            // If the target is far away, and we're not already set to semi auto, switch to semi auto
-            if (___botOwner_0.AimingData.LastDist2Target > SAIN.SAIN.SemiDistance.Value && weapon.SelectedFireMode != Weapon.EFireMode.single)
+            float semidist = SAIN.SAIN.SemiDistance.Value;
+            float autodist = SAIN.SAIN.AutoDistance.Value;
+
+            if (SAIN.SAIN.RecoilModifierFiremodeToggle.Value == true)
+                    {
+                        semidist = (SAIN.SAIN.SemiDistance.Value) / ((weapon.RecoilTotal / SAIN.SAIN.FiremodeRecoilBaseline.Value) * SAIN.SAIN.RecoilModifierFirerate.Value);
+                        autodist = (SAIN.SAIN.AutoDistance.Value) / ((weapon.RecoilTotal / SAIN.SAIN.FiremodeRecoilBaseline.Value) * SAIN.SAIN.RecoilModifierFirerate.Value);
+                    };
+
+            if (___botOwner_0.AimingData.LastDist2Target > semidist && weapon.SelectedFireMode != Weapon.EFireMode.single)
             {
                 Logger.LogDebug($"FireratePatch [{___botOwner_0.name}]: Target is > 20f away, switching to single fire");
                 weapon.FireMode.SetFireMode(Weapon.EFireMode.single);
             }
 
-            // If the target is close by, and we're not already set to full auto, switch to full auto
-            if (___botOwner_0.AimingData.LastDist2Target <= SAIN.SAIN.AutoDistance.Value && weapon.SelectedFireMode != Weapon.EFireMode.fullauto)
+            if (___botOwner_0.AimingData.LastDist2Target <= autodist && weapon.SelectedFireMode != Weapon.EFireMode.fullauto)
             {
                 Logger.LogDebug($"FireratePatch [{___botOwner_0.name}]: Target is <= 20f away, switching to auto");
                 weapon.FireMode.SetFireMode(Weapon.EFireMode.fullauto);
             }
         }
     }
+
 //Allows all bots to use Sniper Scav Logic for scaling fire-rate at distance.
 public class SemiROF : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
             {
-                //return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("method_5");
-                //return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("Shoot");
-                return typeof(GClass544).GetMethod("method_5", BindingFlags.Instance | BindingFlags.NonPublic);
+                return typeof(BotOwner)?.GetProperty("ShootData")?.PropertyType?.GetMethod("method_5", BindingFlags.Instance | BindingFlags.NonPublic);
             }
 
         [PatchPrefix]
          public static bool PatchPrefix(ref BotOwner ___botOwner_0, ref float ___float_4)
             {
-		        if (!___botOwner_0.IsRole(WildSpawnType.marksman))
-		            {
-                        float num = ___botOwner_0.WeaponManager.WeaponAIPreset.HoldTriggerUpTime();
-			            //If target is greater than 20m away, use additional fire-rate calculations previously reserved for sniper scavs
-                        if (___botOwner_0.AimingData.LastDist2Target > SAIN.SAIN.DistanceScalingStart.Value)
-                            {
-                                num = Mathf.Clamp((___botOwner_0.AimingData.RealTargetPoint - ___botOwner_0.Transform.position).magnitude / ___botOwner_0.Settings.FileSettings.Shoot.MARKSMAN_DIST_SEK_COEF, ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MIN, ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MAX);
-                            }
-                        ___float_4 = Time.time + num * UnityEngine.Random.Range(0.5f, 1.5f);
-                        Logger.LogDebug($"Sniper Logic [{___botOwner_0.name}]: Enabling Sniper Logic for fire-rate");
-                        return false;
-		            }
-                Logger.LogDebug($"Sniper Logic [{___botOwner_0.name}]: Ignoring bot for sniper logic");
-                return true;
+                Weapon weapon = ___botOwner_0.WeaponManager.CurrentWeapon;
+                
+                float recoil = 1;
+                float maxwait = ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MAX;
+                float minwait = ___botOwner_0.Settings.FileSettings.Shoot.WAIT_NEXT_SINGLE_SHOT_LONG_MIN;
+                float num = ___botOwner_0.WeaponManager.WeaponAIPreset.HoldTriggerUpTime();
+                float permeter = ___botOwner_0.Settings.FileSettings.Shoot.MARKSMAN_DIST_SEK_COEF;
+    
+                if (SAIN.SAIN.RecoilModifierDistanceToggle.Value == true)
+                    {
+                        recoil = (weapon.RecoilTotal / SAIN.SAIN.FirerateRecoilBaseline.Value) * SAIN.SAIN.RecoilModifierFirerate.Value;
+                    };
+    
+                if (SAIN.SAIN.MaxWaitOverride.Value == true)
+                    {
+                        maxwait = SAIN.SAIN.MaxWaitTime.Value;
+                        minwait = SAIN.SAIN.MinWaitTime.Value;
+                        num = SAIN.SAIN.BaseWaitTime.Value;
+                    };
+    
+                if (SAIN.SAIN.DistancePerMeterOverride.Value == true)
+                    {
+                        permeter = SAIN.SAIN.WaitPerMeter.Value;
+                    };
+    
+			    //Wait Time Logic
+                if (___botOwner_0.AimingData.LastDist2Target > SAIN.SAIN.DistanceScalingStart.Value)
+                    {
+                        num = Mathf.Clamp((___botOwner_0.AimingData.RealTargetPoint - ___botOwner_0.Transform.position).magnitude / (permeter / recoil), minwait, maxwait);
+                    }
+                ___float_4 = Time.time + num * UnityEngine.Random.Range(0.5f, 1.5f);
+                Logger.LogDebug($"Sniper Logic [{___botOwner_0.name}]: Enabling Sniper Logic for fire-rate");
+                return false;
             }    
     }
--- Plugin.cs
+++ Plugin.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using UnityEngine;
 using EFT.InventoryLogic;
 using System.Reflection;
+using HarmonyLib;
 using Aki.Reflection.Patching;
 
 namespace SAIN
@@ -21,22 +22,58 @@ namespace SAIN
     {
         private const String MOD_GUID = "me.sol.aitweaks";
         private const String MOD_NAME = "SAIN";
-        private const String MOD_VERSION = "0.2";
+        private const String MOD_VERSION = "3.5.2";
 
         public static ConfigEntry<bool> Firemode { get; private set; }
         public static ConfigEntry<bool> SemiROF { get; private set; }
+
+        public static ConfigEntry<float> RecoilModifier { get; private set; }
+        public static ConfigEntry<bool> RecoilModifierDistanceToggle { get; private set; }
+        public static ConfigEntry<bool> RecoilModifierFiremodeToggle { get; private set; }
+        public static ConfigEntry<float> RecoilModifierFiremode { get; private set; }
+        public static ConfigEntry<float> FiremodeRecoilBaseline { get; private set; }
+
         public static ConfigEntry<float> SemiDistance { get; private set; }
         public static ConfigEntry<float> AutoDistance { get; private set; }
         public static ConfigEntry<float> DistanceScalingStart { get; private set; }
 
-        public void Awake()
-            {
-                Firemode = Config.Bind("Firemode Swap Settings", "Firemode Toggle", true);               
-                SemiDistance = Config.Bind("Firemode Swap Settings", "Maximum Distance where bots will swap to Semi-Auto", 35f);
-                AutoDistance = Config.Bind("Firemode Swap Settings", "Minimum Distance where bots will swap back to Full-Auto", 30f);
+        public static ConfigEntry<bool> MaxWaitOverride { get; private set; }
+        public static ConfigEntry<bool> DistancePerMeterOverride { get; private set; }
+        public static ConfigEntry<float> MaxWaitTime { get; private set; }
+        public static ConfigEntry<float> MinWaitTime { get; private set; }
+        public static ConfigEntry<float> WaitPerMeter { get; private set; }
+        public static ConfigEntry<float> BaseWaitTime { get; private set; }
+        public static ConfigEntry<float> RecoilModifierFirerate { get; private set; }
+        public static ConfigEntry<float> FirerateRecoilBaseline { get; private set; }
 
-                SemiROF = Config.Bind("Semiauto Distance Scaling Settings", "Distance Scaling Toggle", true);
-                DistanceScalingStart = Config.Bind("Semiauto Distance Scaling Settings", "Distance to start slowing down Semi-Auto Firerate", 30f);
+        private void Awake()
+            {
+                string modeswap = "1. Firemode Swap Settings";
+
+                Firemode = Config.Bind(modeswap, "1 Firemode Toggle", true, "Enables bots swapping to SemiAuto when shooting distant targets");               
+                SemiDistance = Config.Bind(modeswap, "2 Semi Firemode Max Distance", 35f, "Maximum Distance where bots will swap to SemiAuto");
+                AutoDistance = Config.Bind(modeswap, "3 Auto Firemode Min Distance", 30f, "Minimum Distance where bots will swap back to FullAuto");
+                RecoilModifierFiremodeToggle = Config.Bind(modeswap, "4 Recoil for Distance", true, "Enables Recoil Modifiers for Firemode Swap Distance");
+                RecoilModifierFiremode = Config.Bind(modeswap, "5 Recoil Modifier", 1f, "How Much the recoil total of a bots weapon and mods affects fire-rate");
+                FiremodeRecoilBaseline = Config.Bind<float>(modeswap, "6 Total Recoil Baseline", 300f, new ConfigDescription("The amount of total recoil that serves as a baseline for when to start increasing or decreasing Firemode Swap Distance"));
+                //FiremodeRecoilBaseline = Config.Bind<float>("Firemode Swap Settings", "Global ADS FOV Multi", 1f, new ConfigDescription("Applies On Top Of All Other ADS FOV Change Multies. Multiplier For The FOV Change When ADSing. Lower Multi = Lower FOV So More Zoom.", new AcceptableValueRange<float>(0.4f, 1.3f), new ConfigurationManagerAttributes { Order = 10 }));
+                
+                string scaling = "2. Distance Scaling Settings";
+
+                SemiROF = Config.Bind(scaling, "2 Distance Scaling", true, "Distance Scaling Additional Wait Time Toggle");
+                DistanceScalingStart = Config.Bind(scaling, "3 Distance Scaling Start", 20f, "Distance to start slowing down SemiAuto Firerate");
+                RecoilModifierDistanceToggle = Config.Bind(scaling, "4 Recoil for Distance", true, "Enables Recoil Modifiers for SemiAuto distance scaling");
+                RecoilModifierFirerate = Config.Bind(scaling, "5 Recoil Modifier", 1f, "How Much the recoil total of a bots weapon and mods affects fire-rate");
+                FirerateRecoilBaseline = Config.Bind(scaling, "6 Total Recoil Baseline", 300f, new ConfigDescription("The amount of total recoil that serves as a baseline for when to start increasing or decreasing Firerate Scaling"));
+                
+                string diffoverride = "3. Difficulty Overrides";
+
+                MaxWaitOverride = Config.Bind(diffoverride, "1 Override Max and Min Wait Time", true, new ConfigDescription("Override Maximum and Minimum Wait Time for fire-rate Difficulty Settings"));
+                DistancePerMeterOverride = Config.Bind(diffoverride, "2 Distance Per Second Override", true, new ConfigDescription("Override Distance per Seconds of Wait Time for fire-rate Difficulty Setting"));
+                BaseWaitTime = Config.Bind(diffoverride, "3 Base Wait Time", 0.1f, new ConfigDescription("Base Wait Time between shots for all distances"));
+                MaxWaitTime = Config.Bind(diffoverride, "4 Maximum Wait Time", 3f, new ConfigDescription("Max Additional Time to Wait Between Shots"));
+                MinWaitTime = Config.Bind(diffoverride, "5 Minimum Wait Time", 0.01f, new ConfigDescription("Minimum Additional Time to Wait Between Shots"));
+                WaitPerMeter = Config.Bind(diffoverride, "6 Seconds Per X Meter", 100f, new ConfigDescription("For every X meters, add around 1 second of wait time between shots"));
                 
                 new Firemode().Enable();
                 new SemiROF().Enable();
